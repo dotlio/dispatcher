845ada809cfcb8a9cc3fcea0daf9b7900f5cb317
Leonardo Ferreira Guimarães <leonardo@guimaraes.email>
2025-07-02 13:51:04 -0300

    feat: add dispatcher extension for DI
    - Add `AddDispatcher` method in `ServiceCollectionExtensions` to configure DI
    - Register IRequestHandler implementations dynamically from assemblies
    - Add Microsoft.Extensions.DependencyInjection.Abstractions package dependency

--- src/Extensions/ServiceCollectionExtensions.cs
+++ src/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,41 @@
+using System.Reflection;
+using DotLio.Dispatcher.Interfaces;
+using DotLio.Dispatcher.Internals;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace DotLio.Dispatcher.Extensions;
 
-public class ServiceCollectionExtensions
+public static class ServiceCollectionExtensions
 {
-    
+    public static IServiceCollection AddDispatcher(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        services.AddSingleton<IMediator, Mediator>();
+        assemblies = assemblies.Length > 0 ? assemblies : [Assembly.GetEntryAssembly()!];
+        foreach (var assembly in assemblies)
+        {
+            var handlers = assembly.GetTypes()
+                .Where(type => type.IsClass && !type.IsAbstract &&
+                               (type.GetInterfaces().Any(i =>
+                                   i.IsGenericType &&
+                                   (i.GetGenericTypeDefinition() == typeof(IRequestHandler<>) ||
+                                    i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>)))
+                               ))
+                .ToList();
+
+            foreach (var handler in handlers)
+            {
+                var implementedInterfaces = handler.GetInterfaces()
+                    .Where(i => i.IsGenericType &&
+                                (i.GetGenericTypeDefinition() == typeof(IRequestHandler<>) ||
+                                 i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>)));
+
+                foreach (var implementedInterface in implementedInterfaces)
+                {
+                    services.AddTransient(implementedInterface, handler);
+                }
+            }
+        }
+
+        return services;
+    }
 }
